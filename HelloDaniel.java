729a37aeecd781f6186dd1ae9eddb4b98a95e876
HEREY <acrilone18@gmail.com>
2020-11-23 15:27:33 -0600

    SecondCommit

--- HelloDaniel.java
+++ HelloDaniel.java
@@ -2,5 +2,6 @@ public class HelloDaniel {
 	public static void main(String[] args) {
 		System.out.println("Harry Potter");
 		System.out.println("Daniel Garcia"); 
+		System.out.println("QWERTY"); 
 	}
 }
